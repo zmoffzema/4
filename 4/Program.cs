f21d0e274c01982bd2829fd375fa55b72d652457
z_moff <zmoffzema@gmail.com>
2024-06-19 23:52:33 +0300

    Добавьте файлы проекта.

---
+++ 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 1, 3, 7, 2, 9, 5 };
+        int maxValue = FindMaxValue(numbers);
+        Console.WriteLine("The maximum value in the array is: " + maxValue);
+    }
+
+    public static int FindMaxValue(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty");
+        }
+
+        int maxValue = array[0];
+        foreach (int num in array)
+        {
+            if (num > maxValue)
+            {
+                maxValue = num;
+            }
+        }
+
+        return maxValue;
+    }
+}
